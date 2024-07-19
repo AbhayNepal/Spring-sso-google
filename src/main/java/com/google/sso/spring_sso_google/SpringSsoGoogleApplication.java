6f793f330d667230a6b6187131c27269b6e8093f
Abhay Nepal <abhay.nepal.9@gmail.com>
2024-07-19 18:21:58 +0545

    initialized

---
+++ src/main/java/com/google/sso/spring_sso_google/SpringSsoGoogleApplication.java
@@ -0,0 +1,28 @@
+package com.google.sso.spring_sso_google;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@SpringBootApplication
+@RestController
+public class SpringSsoGoogleApplication {
+    @GetMapping
+    public String welcome() {
+        return "Welcome to Spring Sso Google!";
+    }
+
+    @GetMapping("/user")
+    public Principal user(Principal principal) {
+        System.out.println("username: " + principal.getName());
+        return principal;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSsoGoogleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/google/sso/spring_sso_google/SpringSsoGoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.sso.spring_sso_google;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSsoGoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
